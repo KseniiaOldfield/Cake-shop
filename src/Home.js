832b567ca1971e274e2a6b354a5175d99c26dd32
Kseniia <120273816+KseniiaOldfield@users.noreply.github.com>
2025-09-18 11:30:34 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import { data } from "./data"
 import Buttons from "./Buttons";
 import Cakes from "./Cakes";
 import imagecup from "./home-page.jpg";
+import { Link } from "react-router-dom";
 
 function Home() {
     const [cakes, setCakes] = useState(data);
@@ -20,7 +21,7 @@ function Home() {
             <h3>Here you will find a wide variety of cakes,</h3>
             <h3>cupcakes and more.</h3>
             <h3>Book a cake for your date.</h3>
-             <a href="Booknow"><button className='btn'>Book now</button></a>
+             <Link to ="/Booknow"><button className='btn'>Book now</button></Link>
           </div>
         </div>
         < Buttons className= "btn" filteredChoice={chosenCakes} />
